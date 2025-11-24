0481ca56bf6cff6ef619b6e97a2fd1f1eecfb4d0
wyz <wangyuzhuo@arksoft.ltd>
2025-11-24 17:16:29 +0800

    SearchActivity.kt

--- app/src/main/java/com/habit/app/ui/SearchActivity.kt
+++ app/src/main/java/com/habit/app/ui/SearchActivity.kt
@@ -3,6 +3,8 @@ package com.habit.app.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.TextView
@@ -88,7 +90,7 @@ class SearchActivity : BaseActivity() {
                 finish()
                 return@setOnClickListener
             }
-
+            WebViewSearchActivity.startActivity(this)
         }
         binding.ivEngineIcon.setOnClickListener {
             showEngineSelectDialog()
@@ -101,6 +103,13 @@ class SearchActivity : BaseActivity() {
                 }
             }
         }
+        binding.editInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                cancelObserver.value = s.isNullOrEmpty()
+            }
+        })
     }
 
     private fun adjustBottomTool(height: Int) {
---
+++ app/src/main/java/com/habit/app/ui/WebViewSearchActivity.kt
@@ -0,0 +1,72 @@
+package com.habit.app.ui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.habit.app.R
+import com.habit.app.databinding.ActivityWebviewSearchBinding
+import com.habit.app.helper.ThemeManager
+import com.habit.app.ui.base.BaseActivity
+import com.wyz.emlibrary.em.EMManager
+import com.wyz.emlibrary.util.immersiveWindow
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+
+/**
+ * 搜索Activity
+ */
+class WebViewSearchActivity : BaseActivity() {
+    private lateinit var binding: ActivityWebviewSearchBinding
+    private val mScope = MainScope()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebviewSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        immersiveWindow(binding.root, false, binding.containerNavi)
+
+        initView()
+        initData()
+        initListener()
+    }
+
+    private fun initView() {
+        updateUIConfig()
+    }
+
+    private fun initData() {
+
+    }
+
+    private fun initListener() {
+
+    }
+
+    private fun updateUIConfig() {
+        binding.root.setBackgroundColor(ThemeManager.getSkinColor(R.color.page_main_color))
+        binding.ivNaviTabAdd.setImageResource(ThemeManager.getSkinImageResId(R.drawable.iv_navi_tab_add))
+        binding.ivNaviPageRefresh.setImageResource(ThemeManager.getSkinImageResId(R.drawable.iv_navi_page_refresh))
+        binding.ivSearchIcon.setImageResource(ThemeManager.getSkinImageResId(R.drawable.iv_navi_search_icon))
+        binding.editInput.setTextColor(ThemeManager.getSkinColor(R.color.text_main_color_80))
+        EMManager.from(binding.containerInput)
+            .setCorner(18f)
+            .setBorderWidth(1f)
+            .setBorderRealColor(ThemeManager.getSkinColor(R.color.text_main_color_30))
+    }
+
+    override fun onThemeChanged(theme: String) {
+        super.onThemeChanged(theme)
+        updateUIConfig()
+    }
+
+    override fun onDestroy() {
+        mScope.cancel()
+        super.onDestroy()
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            context.startActivity(Intent(context, WebViewSearchActivity::class.java))
+        }
+    }
+}
